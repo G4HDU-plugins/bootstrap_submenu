690fa5d26ee1d5ee54624197da75f382fd8b917b
Barry Keal <barry@keal.me.uk>
2016-08-05 16:48:52 +0100

    Initial

--- languages/English/English_global.php
+++ languages/English/English_global.php
@@ -3,8 +3,8 @@
 // Always use the format LAN_PLUGIN_{FOLDER}_{TYPE} to prevent conflicts. In this case "_BLANK" is the folder. 
 // This should contain the LANs used in the plugin.xml file. 
 
-define("LAN_PLUGIN__BLANK_NAME", "Blank Plugin");
-define("LAN_PLUGIN__BLANK_DIZ",  "A Blank Plugin to help you get started in plugin development. More details can be added here."); 
-define("LAN_PLUGIN__BLANK_LINK", "Blank Link");
+define("LAN_PLUGIN_BSUBMENU_NAME", "Bootstrap SubMenu");
+define("LAN_PLUGIN_BSUBMENU_DIZ",  "A replacement menu for bootstrap themes with multiple sub menus"); 
+
 
 ?>
